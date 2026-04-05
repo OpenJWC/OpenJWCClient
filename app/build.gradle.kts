4de2642627c33d9f8cbba4fd6c2315c7fce614f9
SakiMidare <ssmzhn@163.com>
2026-04-05 23:35:54 +0800

    Bump version code and name
    - Increment `versionCode` to 7 and `versionName` to 0.2.3 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "org.openjwc.client"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.2.2"
+        versionCode = 7
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
